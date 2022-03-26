613d211e9235ebdd448d6da1350630f9fef2a3d2
Juan José <diazsantanajuanjose@gmail.com>
2022-03-26 01:24:43 +0000

    Juanjo - MÉTODOS PAGO:
    - Seeder
    - Pizarra de métodos de pago

--- resources/views/content/admin/metodospago/pizarra.blade.php
+++ resources/views/content/admin/metodospago/pizarra.blade.php
@@ -3,25 +3,25 @@
 @section('content')
 
     <div id="main-metodos">
-        <div class="row">
+        <div class="row match-height">
             @foreach($metodos as $metodo)
                 <div class="col-3">
                     <div class="card">
                         <div class="card-body">
-                            <div class="w-100 text-center d-flex align-items-center" style="height: 10rem;">
+                            <div class="w-100 text-center d-flex align-items-center" style="height: 12rem;">
                                 <img class="img-fluid m-auto" src="{{asset('images/pages/metodos-pago/'.strtolower($metodo->nombre).'.png')}}" style="width: auto; height: 100%;" alt="Card image cap" />
                             </div>
-                            <div class="d-flex justify-content-between mt-3">
+                            <div class="d-flex justify-content-between mt-3 align-items-center">
                                 <h6 class="card-text font-weight-bold">{{$metodo->nombre}}</h6>
-                                <div class="d-flex flex-column align-items-end">
+                                <div class="d-flex flex-row align-items-center" style="height: 2em">
+                                    @if($metodo->nombre != 'Efectivo')
+                                        <button type="button" class="btn btn-flat-primary waves-effect cursor-pointer me-50">
+                                            <i class="fa-solid fa-pen-to-square"></i>
+                                        </button>
+                                    @endif
                                     <div class="custom-control custom-control-primary custom-switch">
-{{--                                        @if($metodo->nombre != 'Efectivo en tienda')--}}
-{{--                                            <button type="button" id="btn-{{$metodo->id}}" data-tipo="{{$metodo->nombre}}"--}}
-{{--                                                    onclick="editMetodo('btn-{{$metodo->id}}')" class="btn p-0 mr-2"--}}
-{{--                                                    @if(!$permiso_editar) disabled @endif><i class="far fa-edit"></i></button>--}}
-{{--                                        @endif--}}
                                         <div class="form-check form-check-primary form-switch">
-                                            <input type="checkbox" checked="" class="form-check-input" id="customSwitch3">
+                                            <input type="checkbox" class="form-check-input cursor-pointer" id="customSwitch3" @if($metodo->estado === 1) checked="" @endif>
                                         </div>
                                     </div>
                                 </div>
@@ -38,10 +38,12 @@
 @endsection
 
 @section('crud-styles')
+{{--    <link href="{{ asset(mix('fonts/font-awesome/css/fontawesome.min.css')) }}"--}}
+{{--          rel="stylesheet" media="print" onload="this.media='all'; this.onload=null;">--}}
 {{--    <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">--}}
 {{--    <link rel="stylesheet" href="{{ asset(mix('vendors/css/file-uploaders/dropzone.min.css')) }}">--}}
 {{--    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-file-uploader.css')) }}">--}}
-    <link rel="stylesheet" href="{{ asset(mix('css/base/pages/ui-feather.css')) }}">
+{{--    <link rel="stylesheet" href="{{ asset(mix('css/base/pages/ui-feather.css')) }}">--}}
     {{-- Flatpicker --}}
 {{--    <link rel="stylesheet" href="{{ asset(mix('vendors/css/pickers/pickadate/pickadate.css')) }}">--}}
 {{--    <link rel="stylesheet" href="{{ asset(mix('vendors/css/pickers/flatpickr/flatpickr.min.css')) }}">--}}
