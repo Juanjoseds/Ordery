0a9f5e2f9c512d3feac52eee832fd39d33961286
Juan José <diazsantanajuanjose@gmail.com>
2022-08-24 22:24:58 +0100

    Juanjo - Home
    - Correcciones estilos

--- resources/views/web/pages/home/slider-tiendas.blade.php
+++ resources/views/web/pages/home/slider-tiendas.blade.php
@@ -18,23 +18,23 @@
                             </div>
                         </div>
                         <div class="d-flex flex-row meetings">
-                            <div class="avatar bg-light-primary rounded me-1">
+                            <div class="avatar bg-light-primary rounded me-1 mb-auto">
                                 <div class="avatar-content">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar avatar-icon font-medium-3"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
                                 </div>
                             </div>
-                            <div class="content-body">
+                            <div class="content-body text-start">
                                 <h6 class="mb-0">Sat, May 25, 2020</h6>
                                 <small>10:AM to 6:PM</small>
                             </div>
                         </div>
                         <div class="d-flex flex-row meetings">
-                            <div class="avatar bg-light-primary rounded me-1">
+                            <div class="avatar bg-light-primary rounded me-1 mb-auto">
                                 <div class="avatar-content">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin avatar-icon font-medium-3"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
                                 </div>
                             </div>
-                            <div class="content-body">
+                            <div class="content-body text-start">
                                 <h6 class="mb-0">Central Park</h6>
                                 <small>Manhattan, New york City</small>
                             </div>
