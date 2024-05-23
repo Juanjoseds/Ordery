9a3804f903186b46a30a3b702d455e0c5dc25d1d
Juanjo <diazsantanajuanjose@gmail.com>
2024-05-24 00:42:35 +0100

    JJ: VISTA TIENDA
    - Mejoras en estilos

--- resources/views/web/pages/home/footer.blade.php
+++ resources/views/web/pages/home/footer.blade.php
@@ -1,11 +1,11 @@
 <div id="footer" class="mt-3">
     <div class="row mt-2">
-        <div class="col-8"></div>
-        <div class="col-2">
+        <div class="col-12 col-lg-8"></div>
+        <div class="col-12 col-lg-2">
             <p class="footer-title">Échale un ojo</p>
             <p>Ver tiendas</p>
         </div>
-        <div class="col-2 footer-politicas">
+        <div class="col-12 col-lg-2 footer-politicas">
             <p>CONDICIONES DE USO</p>
             <p>POLÍTICA DE PRIVACIDAD</p>
             <p>POLÍTICA DE COOKIES</p>
